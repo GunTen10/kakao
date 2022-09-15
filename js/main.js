9a0f522b8e15cd5458ecae33ea391826154dd691
GunTen10 <110226451+GunTen10@users.noreply.github.com>
2022-09-15 11:01:11 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// gnb 서브메뉴 슬라이드
+$('.btn_sub').click(function(){
+    $(this).find('span').toggleClass('on');
+    $(this).siblings('.list_sub_wrap').slideToggle();
+});
+
+// gnb ON
+$('.gnb_btn').click(function(){
+    $('#gnb').animate({left:0},300,'swing');
+    $('.dark').css({'display':'block'});
+});
+// gnb OFF
+$('.dark').click(function(){
+    $('.dark').css({'display':'none'});
+    $('#gnb').animate({left:-304},300,'swing');
+    $('.login_popup').hide();
+});
+
+// 로그인 팝업
+$('.login_require').click(function(){
+    $('.login_popup').show();
+    $('#gnb').animate({left:-304},300,'swing');
+});
+// 로그인 팝업 닫기
+$('.btn_close').click(function(){
+    $('.login_popup').hide();
+    $('.dark').css({'display':'none'});
+});
